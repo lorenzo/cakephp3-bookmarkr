8f367543cc3f08e47c644e369230795731d19a2d
Jose Lorenzo Rodriguez <jose.zap@gmail.com>
2014-10-19 15:59:53 +0200

    Initial skelleton and migration file

---
+++ config/Migrations/20141019135342_initial.php
@@ -0,0 +1,39 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Initial extends AbstractMigration {
+
+	public function change() {
+		$this->table('users')
+			->addColumn('useraname', 'string', ['limit' => 30])
+			->addColumn('password', 'string', ['limit' => 255])
+			->addColumn('created', 'datetime')
+			->addColumn('modified', 'datetime')
+			->create();
+
+		$this->table('bookmarks')
+			->addColumn('user_id', 'integer')
+			->addColumn('title', 'string', ['limit' => 50])
+			->addColumn('description', 'text')
+			->addColumn('url', 'text')
+			->addColumn('created', 'datetime')
+			->addColumn('modified', 'datetime')
+			->create();
+
+		$this->table('tags')
+			->addColumn('title', 'string', ['limit' => 100])
+			->addColumn('created', 'datetime')
+			->addColumn('modified', 'datetime')
+			->create();
+
+		$this->table('bookmarks_tags', ['id' => false, 'primary_key' => ['bookmark_id', 'tag_id']])
+			->addColumn('bookmark_id', 'integer')
+			->addColumn('tag_id', 'integer')
+			->addForeignKey('bookmark_id', 'bookmarks', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
+			->addForeignKey('tag_id', 'tags', 'id', ['delete' => 'CASCADE', 'update' => 'NO_ACTION'])
+			->addIndex(['tag_id', 'bookmark_id'])
+			->create();
+	}
+
+}
